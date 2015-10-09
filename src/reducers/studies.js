f97c52ad6a70d916f66254d7df63aa67f2176b69
Oren <orengolan@gmail.com>
2015-10-09 12:53:54 -0700

    getStudies as a function

--- src/reducers/studies.js
+++ src/reducers/studies.js
@@ -3,10 +3,14 @@ import { createReducer } from 'utils';
 // normally this would be imported from /constants, but in trying to keep
 // this starter kit as small as possible we'll just define it here.
 const FETCH_STUDIES = 'FETCH_STUDIES';
-
 const initialState = { studies: [] };
-export default createReducer(initialState, {
-  [FETCH_STUDIES] : () => ({ studies: [ { name: 'study 1'}, {name: 'study 2'}] })
-});
 
+const getStudies = () => {
+  studies: [ { name: 'study 3'}, {name: 'study 4'}];
+};
+
+export default createReducer(initialState, {
+  // [FETCH_STUDIES] : () => ({ studies: [ { name: 'study 1'}, {name: 'study 2'}] })
+  [FETCH_STUDIES] : () => getStudies()
+});
 
